3d1180d655ef1d5ba731c3b206f160a2238c33ff
Matthew Newville <newville@cars.uchicago.edu>
2020-11-01 10:42:53 -0600

    set version to 0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name = 'newportxps',
-      version = '0.1',
+      version = '0.2',
       author = 'Matthew Newville',
       author_email = 'newville@cars.uchicago.edu',
       license = 'BSD',
       description = 'Python interface to Newport XPS controllers',
       packages = ['newportxps'],
-	  install_requires = ['pysftp']
+      install_requires = ['pysftp']
       )
